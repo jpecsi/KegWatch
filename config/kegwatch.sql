f9a763b510fafa300f1ffaf27ad5338e5ed8ab0f
Joe Pecsi <jpecsi@me.com>
2022-10-10 12:02:25 -0400

    Updated db schema, new method for reading barcode scanner, added game mode

--- config/kegwatch.sql
+++ config/kegwatch.sql
@@ -1,66 +1,81 @@
---
--- Table structure for table `beer_log`
---
-
 DROP TABLE IF EXISTS `beer_log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `beer_log` (
   `time` datetime NOT NULL,
-  `tap` int DEFAULT NULL,
-  `beer_name` varchar(255) DEFAULT NULL,
-  `oz_poured` float DEFAULT NULL,
+  `tap_id` int DEFAULT NULL,
+  `beer_id` varchar(100) DEFAULT NULL,
+  `beer_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `consumer` varchar(255) DEFAULT NULL,
-  `oz_remain` float DEFAULT NULL,
-  `date_tapped` varchar(100) DEFAULT NULL,
+  `oz_poured` float DEFAULT NULL,
   PRIMARY KEY (`time`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
+
 
 --
--- Dumping data for table `beer_log`
+-- Table structure for table `consumers`
+--
+DROP TABLE IF EXISTS `consumers`;
+CREATE TABLE `consumers` (
+  `id` varchar(255) NOT NULL,
+  `body_cat` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+
+--
+-- Table structure for table `game_log`
 --
 
-LOCK TABLES `beer_log` WRITE;
-/*!40000 ALTER TABLE `beer_log` DISABLE KEYS */;
-/*!40000 ALTER TABLE `beer_log` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `game_log`;
+CREATE TABLE `game_log` (
+  `game` varchar(255) NOT NULL,
+  `time` datetime NOT NULL,
+  `consumer` varchar(255) NOT NULL,
+  `type_consumed` varchar(255) NOT NULL,
+  `amount_consumed` float NOT NULL,
+  `bev_abv` float DEFAULT NULL,
+  `team` varchar(255) DEFAULT NULL
+);
 
+--
+-- Table structure for table `games`
+--
+
+DROP TABLE IF EXISTS `games`;
+CREATE TABLE `games` (
+  `id` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL,
+  `players` int DEFAULT NULL,
+  `status` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
 
 
 --
 -- Table structure for table `keg_log`
 --
-
 DROP TABLE IF EXISTS `keg_log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `keg_log` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `beer_name` varchar(100) DEFAULT NULL,
+  `id` varchar(255) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `tap` int DEFAULT NULL,
+  `abv` float DEFAULT NULL,
+  `capacity` float DEFAULT NULL,
+  `remaining` float DEFAULT NULL,
   `date_tapped` varchar(100) DEFAULT NULL,
   `date_kicked` varchar(100) DEFAULT NULL,
   `days_to_consume` int DEFAULT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
 --
--- Dumping data for table `keg_log`
+-- Table structure for table `teams`
 --
 
-LOCK TABLES `keg_log` WRITE;
-/*!40000 ALTER TABLE `keg_log` DISABLE KEYS */;
-/*!40000 ALTER TABLE `keg_log` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-09-23  3:18:19
+DROP TABLE IF EXISTS `teams`;
+CREATE TABLE `teams` (
+  `game` varchar(255) NOT NULL,
+  `consumer` varchar(255) NOT NULL,
+  `team` varchar(255) NOT NULL
+);
--- sensor.py
+++ sensor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # ========== LIBRARIES ========== #
+from asyncore import write
 import time, os
 import RPi.GPIO as GPIO 
 from datetime import datetime
@@ -9,23 +10,69 @@ import paho.mqtt.client as mqtt
 from threading import Thread
 import configparser
 import mysql.connector
-
+import evdev
+from evdev import InputDevice, categorize
 
 
 # ========== FUNCTIONS ========== #
-# Dirty, hacky way to keep the sensor code running
-def persist():
-    while True:
-        time.sleep(1)
-
-
-
 # Thread to read barcode input (continuously)
 def scan_barcode():
+
+    # ASCII Scan Codes (lowercase)
+    scancodes = {
+        0: None, 1: u'ESC', 2: u'1', 3: u'2', 4: u'3', 5: u'4', 6: u'5', 7: u'6', 8: u'7', 9: u'8',
+        10: u'9', 11: u'0', 12: u'-', 13: u'=', 14: u'BKSP', 15: u'TAB', 16: u'q', 17: u'w', 18: u'e', 19: u'r',
+        20: u't', 21: u'y', 22: u'u', 23: u'i', 24: u'o', 25: u'p', 26: u'[', 27: u']', 28: u'CRLF', 29: u'LCTRL',
+        30: u'a', 31: u's', 32: u'd', 33: u'f', 34: u'g', 35: u'h', 36: u'j', 37: u'k', 38: u'l', 39: u';',
+        40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'z', 45: u'x', 46: u'c', 47: u'v', 48: u'b', 49: u'n',
+        50: u'm', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 57: u' ', 100: u'RALT'
+    }
+
+    # ASCII Scan Codes (uppercase)
+    capscodes = {
+        0: None, 1: u'ESC', 2: u'!', 3: u'@', 4: u'#', 5: u'$', 6: u'%', 7: u'^', 8: u'&', 9: u'*',
+        10: u'(', 11: u')', 12: u'_', 13: u'+', 14: u'BKSP', 15: u'TAB', 16: u'Q', 17: u'W', 18: u'E', 19: u'R',
+        20: u'T', 21: u'Y', 22: u'U', 23: u'I', 24: u'O', 25: u'P', 26: u'{', 27: u'}', 28: u'CRLF', 29: u'LCTRL',
+        30: u'A', 31: u'S', 32: u'D', 33: u'F', 34: u'G', 35: u'H', 36: u'J', 37: u'K', 38: u'L', 39: u':',
+        40: u'\'', 41: u'~', 42: u'LSHFT', 43: u'|', 44: u'Z', 45: u'X', 46: u'C', 47: u'V', 48: u'B', 49: u'N',
+        50: u'M', 51: u'<', 52: u'>', 53: u'?', 54: u'RSHFT', 56: u'LALT',  57: u' ', 100: u'RALT'
+    }
+   
+    # Setup starting variables
     global cuser
     cuser = "Anonymous"
-    while True:
-        cuser = input("[" + str(datetime.now()) + "] READY TO SCAN BARCODE ")
+    temp = ""
+    caps = False
+
+    # Grab the barcode scanner (exclusively)
+    dev.grab()
+  
+    # Run a loop looking for key presses
+    for event in dev.read_loop():
+        if event.type == evdev.ecodes.EV_KEY:
+            data = evdev.categorize(event)  # Save the event temporarily to introspect it
+            # Only grab "down" presses; identify if lower/uppercase character
+            if data.scancode == 42:  # Down events only
+                if data.keystate == 1:
+                    caps = True
+                if data.keystate == 0:
+                    caps = False
+            if data.keystate == 1:  # Down events only
+                if caps:
+                    key_lookup = u'{}'.format(capscodes.get(data.scancode)) or u'UNKNOWN:[{}]'.format(data.scancode)  # Lookup or return UNKNOWN:XX
+                else:
+                    key_lookup = u'{}'.format(scancodes.get(data.scancode)) or u'UNKNOWN:[{}]'.format(data.scancode)  # Lookup or return UNKNOWN:XX
+                if (data.scancode != 42) and (data.scancode != 28):
+                    temp += key_lookup  
+                if(data.scancode == 28):
+                    # Set the user to the scanned ID, reset the temp/builder string
+                    cuser=temp
+                    temp=""
+
+                    # Catch game-mode barcodes
+                    if cuser[0] == '0':
+                        if config.getboolean("game_mode", "enabled"):
+                            game_logic(cuser[1:],"Mixed Drink",8,config.getfloat("game_mode","average_abv"))
 
 
 
@@ -38,7 +85,7 @@ def write_to_db(q,v):
             port=config.getint("database","port"),
             user=config['database']['username'],
             password=config['database']['password'],
-            database=config['database']['db_name']
+            database=config['database']['database']
         )
 
         # Cursor to execute SQL
@@ -54,6 +101,57 @@ def write_to_db(q,v):
     # Close the connection
     db.close()
     db_server.close()
+
+
+
+def read_db(q,v):
+    try:
+        # Connect to Database
+        db_server = mysql.connector.connect(
+            host=config['database']['host'],
+            port=config.getint("database","port"),
+            user=config['database']['username'],
+            password=config['database']['password'],
+            database=config['database']['database']
+        )
+
+        # Cursor to execute SQL
+        db = db_server.cursor()
+
+        # Write to the database
+        results = []
+        db.execute(q,v)
+        for row in db:
+            results.append(row)
+        
+    except Exception as e:
+        print("DATABASE ERROR: " + str(e))
+
+    # Close the connection
+    db.close()
+    db_server.close()
+    return results
+
+
+
+# Put together the proper data for game sessions
+def game_logic(c,t,q,p):
+    # Get the active game id
+    game = read_db("SELECT id FROM games WHERE status=%s", (1,))
+    game = game[0][0]
+
+    # Get current time
+    now = datetime.now()
+    
+    # Get the team name (if there is one)
+    team = read_db("SELECT team FROM teams WHERE consumer=%s AND game=%s", (c,game))
+    if len(team) == 0:
+        team = "None"
+    else:
+        team = team[0][0]
+
+    # Log the drink to the game table
+    write_to_db("INSERT INTO game_log (game,time,consumer,type_consumed,amount_consumed,bev_abv,team) VALUES (%s,%s,%s,%s,%s,%s,%s)", (game,now,c,t,q,p,team))
 
 
 
@@ -97,42 +195,26 @@ def tap2(channel):
 
 
 
-# Log the keg once kicked
-def log_keg(t):
-    # Get current tap data
-    tap = {
-        "beer": config[taps[t]]['beer_name'],
-        "tapped": config[taps[t]]['date_tapped']
-    }
-
-    # Calcualte how many days since the keg was tapped
-    today = date(datetime.now().year,datetime.now().month,datetime.now().day)
-    tapped = tap["tapped"].split("-")
-    tapped_date = date(int(tapped[0]),int(tapped[1]),int(tapped[2]))
-    delta = today-tapped_date
-
-    # Write to database
-    write_to_db("INSERT INTO keg_log (beer_name,date_tapped,date_kicked,days_to_consume) VALUES (%s,%s,%s,%s)",(tap["beer"],tapped_date,today,delta.days))
-
-
-
 # Calculate Beer Remaining
 def calc_beer(t,s):
     # Get current time to report "last pour time"
     now = datetime.now()
 
-    # Get current tap data
-    tap = {
-        "beer": config[taps[t]]['beer_name'],
-        "remaining": config.getfloat(taps[t],"keg_remaining"),
-        "flow": config.getfloat(taps[t],"flow_rate"),
-        "tapped": config[taps[t]]['date_tapped'],
-        "active": config.getint(taps[t],"active")
-    }
-    
-    # Make sure the keg isn't already kicked...
-    if tap["active"] == 1:
-        # Who poured the beer?
+    # Get tap information
+    db_data = read_db("SELECT id,name,abv,remaining,date_tapped FROM keg_log WHERE tap=%s AND status=1", (t,))
+
+    # Proceed if the tap is active...
+    if len(db_data) == 1:
+        # Get current tap data
+        tap = {
+            "beer_id": db_data[0][0],
+            "beer_name": db_data[0][1],
+            "abv": db_data[0][2],
+            "remaining": db_data[0][3],
+            "date_tapped": db_data[0][4],
+            "flow": config.getfloat(("tap_"+str(t)),"flow_rate"),
+        }
+
         global cuser
         if len(cuser) < 1:
             cuser = "Anonymous"
@@ -149,48 +231,49 @@ def calc_beer(t,s):
 
         # Update beer remaining
         if beer_remaining == 0:
-            log_keg(t)
-            config.set(taps[t],'active','0')
+            # Calcualte how many days since the keg was tapped
+            today = date(datetime.now().year,datetime.now().month,datetime.now().day)
+            tapped = tap["date_tapped"].split("-")
+            tapped_date = date(int(tapped[0]),int(tapped[1]),int(tapped[2]))
+            delta = today-tapped_date
 
-        # Update settings.conf
-        config.set(taps[t], 'keg_remaining', str(beer_remaining))
-        with open(cf, 'w') as configfile:
-            config.write(configfile)
+            # Write to database
+            write_to_db("UPDATE keg_log SET remaining=0,date_kicked=%s,days_to_consume=%s,status=0 WHERE id=%s", (today,delta.days,tap["beer_id"]))
 
          # Log in database
-        write_to_db("INSERT INTO beer_log (time,tap,beer_name,oz_poured,consumer,oz_remain,date_tapped) VALUES (%s,%s,%s,%s,%s,%s,%s)",(now,t,tap["beer"],beer_poured,consumer,beer_remaining,tap["tapped"]))
-        
-        # Update MQTT
-        mqtt_publish(t)
-    
+        write_to_db("INSERT INTO beer_log (time,tap_id,beer_id,beer_name,consumer,oz_poured) VALUES (%s,%s,%s,%s,%s,%s)", (now,t,tap["beer_id"],tap["beer_name"],consumer,beer_poured))
+        write_to_db("UPDATE keg_log SET remaining=%s WHERE id=%s",(beer_remaining,tap["beer_id"]))
 
+        # If game mode is enabled...
+        if config.getboolean("game_mode", "enabled"):
+            drink_type = ("(Beer) " + tap["beer_name"])
+            game_logic(consumer,drink_type,beer_poured,tap["abv"])
+
+        # Update MQTT
+        mqtt_publish(tap)
+
+    
 
 # Push data to Home Assistant via MQTT
 def mqtt_publish(t):
-    # Connect to MQTT Server
-    m_client = mqtt.Client(config['mqtt_broker']['client_id'])
-    m_client.username_pw_set(username=config['mqtt_broker']['username'],password=config['mqtt_broker']['password'])
-    m_client.connect(config['mqtt_broker']['host'],config.getint("mqtt_broker","port"))
+    # Make sure MQTT is enabled
+    if (config.getboolean("mqtt_broker", "enabled")):
+        # Connect to MQTT Server
+        m_client = mqtt.Client(config['mqtt_broker']['client_id'])
+        m_client.username_pw_set(username=config['mqtt_broker']['username'],password=config['mqtt_broker']['password'])
+        m_client.connect(config['mqtt_broker']['host'],config.getint("mqtt_broker","port"))
 
-    # Load data
-    tap = {
-        "beer": config[taps[t]]['beer_name'],
-        "capacity": config.getfloat(taps[t],"keg_capacity"),
-        "remaining": config.getfloat(taps[t],"keg_remaining"),
-        "flow": config.getfloat(taps[t],"flow_rate"),
-    }
+        # Convert oz to "beers"
+        beers_rem = round(t["remaining"]/12,1)
 
-    # Convert oz to "beers"
-    beers_rem = round(tap["remaining"]/12,1)
+        # Build topic strings
+        root_topic = config['mqtt_topics']["root_topic"] + "/t" + str(t) + "-"
 
-    # Build topic strings
-    root_topic = config['mqtt_topics']["root_topic"] + "/" + config[taps[t]]["mqtt_topic_id"] + "-"
-
-    # Publish Data
-    m_client.publish((root_topic+config['mqtt_topics']["beer_topic"]),tap["beer"])
-    m_client.publish((root_topic+config['mqtt_topics']["keg_capacity_topic"]),tap["capacity"])
-    m_client.publish((root_topic+config['mqtt_topics']["keg_remain_topic"]),tap["remaining"])
-    m_client.publish((root_topic+config['mqtt_topics']["beers_remain_topic"]),beers_rem)
+        # Publish Data
+        m_client.publish((root_topic+config['mqtt_topics']["beer_topic"]),t["beer"])
+        m_client.publish((root_topic+config['mqtt_topics']["keg_capacity_topic"]),t["capacity"])
+        m_client.publish((root_topic+config['mqtt_topics']["keg_remain_topic"]),t["remaining"])
+        m_client.publish((root_topic+config['mqtt_topics']["beers_remain_topic"]),beers_rem)
 
 
 
@@ -218,12 +301,6 @@ if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read(cf)
 
-    # Collection of taps
-    taps = {
-        1: "tap_1",
-        2: "tap_2"
-    }
-
     # Hardware Configuration
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
@@ -242,9 +319,9 @@ if __name__ == '__main__':
     GPIO.setup(t2_led,GPIO.OUT)                                                 # Configure the LED
     GPIO.add_event_detect(t2_gpio, GPIO.BOTH, callback=tap2,bouncetime=300)     # Handler to listen for switch
 
-    # Persist Service
-    #p_thread = Thread(target=persist)
-    #p_thread.start()
+    # Grab the barcode scanner
+    dev_path = "/dev/input/" + config.get("hardware","barcode_dev")
+    dev = InputDevice(dev_path)
 
     # Thread for barcode input
     i_thread = Thread(target=scan_barcode)
